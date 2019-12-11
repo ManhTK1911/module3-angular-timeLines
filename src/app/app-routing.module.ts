58da8b7d63c38ffc4baae6ab709d748d61fb22f8
ManhTK1911 <manhtk1911@gmail.com>
2019-12-11 21:51:45 +0700

    timelines

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
+import {TimelinesComponent} from './timelines/timelines.component';
+import {NgModule} from '@angular/core';
 
+const routes: Routes = [
+  {
+    path: 'timelines',
+    component: TimelinesComponent
 
-const routes: Routes = [];
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
